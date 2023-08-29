e4e9b3d1e72b4caaabbb3fcb4841843e02b91513
rigbeats <rishat.gabdullin2016@mail.ru>
2023-08-29 21:28:05 +0600

    Реализовал внопки слева

--- Finance-tracker/Form1.cs
+++ Finance-tracker/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +13,9 @@ namespace Finance_tracker
 {
     public partial class Form1 : Form
     {
+        string projectPath = Path.GetFullPath(Path.Combine(Application.StartupPath, "..\\.."));
+        (PictureBox, Label, string) selectedButton;
+
         public Form1()
         {
             InitializeComponent();
@@ -19,25 +23,145 @@ namespace Finance_tracker
 
         private void Form1_Load(object sender, EventArgs e)
         {
+            string imagePath = Path.Combine(projectPath, "Icons\\selected_compass_icon.png");
 
+            tabControl.SelectedTab = Overview;
+            pbOverview.Image = Image.FromFile(imagePath);
+            lOverview.ForeColor = Color.FromArgb(36, 94, 231);
+
+            selectedButton.Item1 = pbOverview;
+            selectedButton.Item2 = lOverview;
+            selectedButton.Item3 = imagePath;
         }
 
-        private void label3_Click(object sender, EventArgs e)
+        private void DeselectButton()
         {
+            string imagePath = selectedButton.Item3;
+            imagePath = imagePath.Replace("selected_", "");
+            selectedButton.Item1.Image = Image.FromFile(imagePath);
+            selectedButton.Item2.ForeColor = Color.FromArgb(74, 85, 103);
         }
 
-        private void label4_Click(object sender, EventArgs e)
+        private void SelectButton(string path, PictureBox pictureBox, Label label, TabPage tabPage)
         {
+            string imagePath = Path.Combine(projectPath, path);
+
+            tabControl.SelectedTab = tabPage;
+            pictureBox.Image = Image.FromFile(imagePath);
+            label.ForeColor = Color.FromArgb(36, 94, 231);
+
+            //сохранение нажатой кнопки
+            selectedButton.Item1 = pictureBox;
+            selectedButton.Item2 = label;
+            selectedButton.Item3 = imagePath;
         }
 
-        private void Card_Click(object sender, EventArgs e)
+        private void BOverview_Click(object sender, EventArgs e)
         {
+            DeselectButton();
 
+            SelectButton(
+                "Icons\\selected_compass_icon.png",
+                pbOverview,
+                lOverview,
+                Overview
+                );
         }
 
-        private void tableLayoutPanel1_Paint(object sender, PaintEventArgs e)
+        private void BCard_Click(object sender, EventArgs e)
         {
+            DeselectButton();
 
+            SelectButton(
+                "Icons\\selected_credit_card_icon.png",
+                pbCard,
+                lCard,
+                Card
+                );
+        }
+
+        private void BBudget_Click(object sender, EventArgs e)
+        {
+            DeselectButton();
+
+            SelectButton(
+                "Icons\\selected_dollar_sign_icon.png",
+                pbBudget,
+                lBudget,
+                Budget
+                );
+        }
+
+        private void BPayment_Click(object sender, EventArgs e)
+        {
+            DeselectButton();
+
+            SelectButton(
+                "Icons\\selected_money_icon.png",
+                pbPayment,
+                lPayment,
+                Payment
+                );
+        }
+
+        private void BStatistics_Click(object sender, EventArgs e)
+        {
+            DeselectButton();
+
+            SelectButton(
+                "Icons\\selected_statistics_icon.png",
+                pbStatistics,
+                lStatistics,
+                Statistics
+                );
+        }
+
+        private void BReport_Click(object sender, EventArgs e)
+        {
+            DeselectButton();
+
+            SelectButton(
+                "Icons\\selected_save_icon.png",
+                pbReport,
+                lReport,
+                Report
+                );
+        }
+
+        private void BAccount_Click(object sender, EventArgs e)
+        {
+            DeselectButton();
+
+            SelectButton(
+                "Icons\\selected_user_icon.png",
+                pbAccount,
+                lAccount,
+                Account
+                );
+        }
+
+        private void BLogout_Click(object sender, EventArgs e)
+        {
+            DeselectButton();
+
+            SelectButton(
+                "Icons\\selected_user_minus_icon.png",
+                pbLogout,
+                lLogout,
+                Logout
+                );
+        }
+
+        private void BSettings_Click(object sender, EventArgs e)
+        {
+            DeselectButton();
+
+            SelectButton(
+                "Icons\\selected_settings_icon.png",
+                pbSettings,
+                lSettings,
+                Settings
+                );
         }
     }
 }
