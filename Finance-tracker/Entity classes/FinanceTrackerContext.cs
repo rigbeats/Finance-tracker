fc0966093a31cefb0569c7d232b60d6f75e29c8c
rigbeats <rishat.gabdullin2016@mail.ru>
2023-09-02 20:50:29 +0600

    Класс контекста для с вязи с БД

---
+++ Finance-tracker/Entity classes/FinanceTrackerContext.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Finance_tracker.Entity_classes
+{
+    public class FinanceTrackerContext : DbContext
+    {
+        public FinanceTrackerContext() : base("FinanceTrackerConnection")
+        {
+        }
+
+        public DbSet<User> Users { get; set; }
+
+        public DbSet<Card> Cards { get; set; }
+
+        public DbSet<Transaction> Transactions { get; set; }
+    }
+}
