82c44c6fa9e4efc052b7383837e5383926cf9cfc
rigbeats <rishat.gabdullin2016@mail.ru>
2023-09-10 15:53:27 +0600

    Добавил функцию сохранения транзакций в PDF файле

--- Finance-tracker/Form1.cs
+++ Finance-tracker/Form1.cs
@@ -11,6 +11,7 @@ using System.Windows.Forms;
 using Finance_tracker.Entity_classes;
 using iTextSharp.text;
 using iTextSharp.text.pdf;
+using Org.BouncyCastle.Utilities;
 
 namespace Finance_tracker
 {
@@ -421,65 +422,115 @@ namespace Finance_tracker
         {
             if (saveFileDialog.ShowDialog() == DialogResult.OK)
             {
-                string pdfPath = saveFileDialog.FileName;
-
-                var doc = new Document();
-                var writer = PdfWriter.GetInstance(doc,
-                    new FileStream(pdfPath, FileMode.Create));
-
-                doc.Open();
-
-                var paragraph = new Paragraph("Account statement");
-                paragraph.Alignment = Element.ALIGN_CENTER;
-
-                doc.Add(paragraph);
-                doc.Add(new Chunk());
-
-                PdfPTable table = new PdfPTable(4);
-
-                table.AddCell("Amount");
-                table.AddCell("Transaction");
-                table.AddCell("Category");
-                table.AddCell("Date");
-
-                var transactions = new List<Transaction>();
-                using (var context = new FinanceTrackerContext())
-                {
-                    var currentDate = DateTime.Now;
-                    var initialDate = new DateTime();
-
-                    switch (selectedPeriod)
-                    {
-                        case "week":
-                            initialDate = currentDate.AddDays(-7);
-                            break;
-                        case "month":
-                            initialDate = currentDate.AddMonths(-1);
-                            break;
-                        case "year":
-                            initialDate = currentDate.AddYears(-1);
-                            break;
-                    }
-
-                    transactions = context.Transactions
-                        .Where(x => x.Date >= initialDate
-                            && x.Date <= currentDate)
-                        .ToList();
-                }
-
-                foreach (var transaction in transactions)
-                {
-                    table.AddCell(Convert.ToString(transaction.Amount));
-                    table.AddCell(transaction.Target);
-                    table.AddCell(transaction.Category);
-                    table.AddCell(Convert.ToString(transaction.Date));
-                }
-
-                doc.Add(table);
-
-                doc.Close();
+                CreatePdfFile();
             }
         }
+
+        private void CreatePdfFile()
+        {
+            string pdfPath = saveFileDialog.FileName;
+
+            var doc = new Document();
+            var writer = PdfWriter.GetInstance(doc,
+                new FileStream(pdfPath, FileMode.Create));
+
+            doc.Open();
+
+            var font = FontFactory.GetFont("Arial", 16);
+            var paragraph = new Paragraph("Account statement", font);
+            paragraph.Alignment = Element.ALIGN_CENTER;
+
+            doc.Add(paragraph);
+            doc.Add(new Chunk());
+
+            var table = CreateTableWithTransactions();
+            doc.Add(table);
+
+            doc.Close();
+        }
+
+        private PdfPTable CreateTableWithTransactions()
+        {
+            var table = new PdfPTable(4);
+            var headers = new string[]
+            {
+                "Amount",
+                "Transaction",
+                "Category",
+                "Date"
+            };
+
+            foreach (var header in headers)
+            {
+                table.AddCell(GetCellWithText(header, true));
+            }
+
+            var transactions = GetLastTransactions();
+            foreach (var transaction in transactions)
+            {
+                var date = transaction.Date;
+
+                table.AddCell(GetCellWithText(transaction.Amount));
+                table.AddCell(GetCellWithText(transaction.Target));
+                table.AddCell(GetCellWithText(transaction.Category));
+                table.AddCell(GetCellWithText($"{date.Day}.{date.Month}.{date.Year}"));
+            }
+
+            return table;
+        }
+
+        private DateTime GetInitialDate(string selectedPeriod)
+        {
+            var currentDate = DateTime.Now;
+            DateTime initialDate;
+
+            switch (selectedPeriod)
+            {
+                case "week":
+                    initialDate = currentDate.AddDays(-7);
+                    break;
+                case "month":
+                    initialDate = currentDate.AddMonths(-1);
+                    break;
+                default:
+                    initialDate = currentDate.AddYears(-1);
+                    break;
+            }
+
+            return initialDate;
+        }
+
+        private List<Transaction> GetLastTransactions()
+        {
+            var transactions = new List<Transaction>();
+
+            using (var context = new FinanceTrackerContext())
+            {
+                var currentDate = DateTime.Now;
+                var initialDate = GetInitialDate(selectedPeriod);
+
+                transactions = context.Transactions
+                    .Where(x => x.Date >= initialDate
+                        && x.Date <= currentDate)
+                    .ToList();
+            }
+
+            return transactions;
+        }
+
+        private PdfPCell GetCellWithText<T>(T content, bool verticalAlignment = false)
+        {
+            var phraze = new Phrase(Convert.ToString(content));
+            var cell = new PdfPCell(phraze);
+            cell.FixedHeight = 25;
+            if (verticalAlignment)
+                cell.HorizontalAlignment = Element.ALIGN_CENTER;
+
+            return cell;
+        }
+
+
+
 
 
 
