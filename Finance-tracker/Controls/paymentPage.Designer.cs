c8c62509a19d93f6f41f9d94c37b07580a5f2024
rigbeats <rishat.gabdullin2016@mail.ru>
2023-09-17 20:08:16 +0600

    Добавил новые контролы

---
+++ Finance-tracker/Controls/paymentPage.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace Finance_tracker.Controls
+{
+    partial class paymentPage
+    {
+        /// <summary> 
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором компонентов
+
+        /// <summary> 
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label11 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.saveFile1 = new Finance_tracker.Controls.SaveFile();
+            this.SuspendLayout();
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Font = new System.Drawing.Font("Helvetica", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label11.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(127)))), ((int)(((byte)(138)))), ((int)(((byte)(155)))));
+            this.label11.Location = new System.Drawing.Point(12, 51);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(250, 18);
+            this.label11.TabIndex = 112;
+            this.label11.Text = "Here you can save the data to a file";
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.BackColor = System.Drawing.Color.White;
+            this.label12.Font = new System.Drawing.Font("Helvetica Bold", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label12.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(17)))), ((int)(((byte)(23)))), ((int)(((byte)(41)))));
+            this.label12.Location = new System.Drawing.Point(10, 17);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(80, 25);
+            this.label12.TabIndex = 111;
+            this.label12.Text = "Report";
+            // 
+            // saveFile1
+            // 
+            this.saveFile1.Location = new System.Drawing.Point(15, 78);
+            this.saveFile1.Name = "saveFile1";
+            this.saveFile1.Size = new System.Drawing.Size(658, 448);
+            this.saveFile1.TabIndex = 114;
+            // 
+            // paymentPage
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.Controls.Add(this.saveFile1);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label12);
+            this.Name = "paymentPage";
+            this.Size = new System.Drawing.Size(1053, 681);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label12;
+        private SaveFile saveFile1;
+    }
+}
