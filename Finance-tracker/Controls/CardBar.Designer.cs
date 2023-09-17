0d0c2dd332e5d92e1d6068e2bd6a69dda9a7487a
rigbeats <rishat.gabdullin2016@mail.ru>
2023-09-17 15:20:47 +0600

    Добавил выбор карты и возможность создания/удаления

--- Finance-tracker/Controls/CardBar.Designer.cs
+++ Finance-tracker/Controls/CardBar.Designer.cs
@@ -28,8 +28,14 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(CardBar));
             this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.pbAdditionaSettings = new System.Windows.Forms.PictureBox();
+            this.bAdd = new System.Windows.Forms.Button();
+            this.pbDot1 = new System.Windows.Forms.PictureBox();
+            this.pbDot3 = new System.Windows.Forms.PictureBox();
+            this.pbDot2 = new System.Windows.Forms.PictureBox();
             this.tbValidThru = new System.Windows.Forms.TextBox();
             this.tbCardHolder = new System.Windows.Forms.TextBox();
             this.tbCardNumber = new System.Windows.Forms.TextBox();
@@ -38,13 +44,26 @@
             this.lCardTitle1 = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
             this.pbLargeCard = new System.Windows.Forms.PictureBox();
+            this.AddDeleteCard = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.addToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.deleteToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pbAdditionaSettings)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbDot1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbDot3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbDot2)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pbLargeCard)).BeginInit();
+            this.AddDeleteCard.SuspendLayout();
             this.SuspendLayout();
             // 
             // groupBox1
             // 
             this.groupBox1.BackColor = System.Drawing.Color.White;
+            this.groupBox1.Controls.Add(this.pbAdditionaSettings);
+            this.groupBox1.Controls.Add(this.bAdd);
+            this.groupBox1.Controls.Add(this.pbDot1);
+            this.groupBox1.Controls.Add(this.pbDot3);
+            this.groupBox1.Controls.Add(this.pbDot2);
             this.groupBox1.Controls.Add(this.tbValidThru);
             this.groupBox1.Controls.Add(this.tbCardHolder);
             this.groupBox1.Controls.Add(this.tbCardNumber);
@@ -58,6 +77,68 @@
             this.groupBox1.Size = new System.Drawing.Size(338, 274);
             this.groupBox1.TabIndex = 3;
             this.groupBox1.TabStop = false;
+            // 
+            // pbAdditionaSettings
+            // 
+            this.pbAdditionaSettings.BackColor = System.Drawing.Color.White;
+            this.pbAdditionaSettings.Image = ((System.Drawing.Image)(resources.GetObject("pbAdditionaSettings.Image")));
+            this.pbAdditionaSettings.Location = new System.Drawing.Point(281, 23);
+            this.pbAdditionaSettings.Name = "pbAdditionaSettings";
+            this.pbAdditionaSettings.Size = new System.Drawing.Size(25, 25);
+            this.pbAdditionaSettings.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbAdditionaSettings.TabIndex = 129;
+            this.pbAdditionaSettings.TabStop = false;
+            this.pbAdditionaSettings.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pbAdditionaSettings_MouseDown);
+            // 
+            // bAdd
+            // 
+            this.bAdd.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(36)))), ((int)(((byte)(95)))), ((int)(((byte)(232)))));
+            this.bAdd.Font = new System.Drawing.Font("Helvetica", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bAdd.ForeColor = System.Drawing.Color.White;
+            this.bAdd.Location = new System.Drawing.Point(221, 240);
+            this.bAdd.Name = "bAdd";
+            this.bAdd.Size = new System.Drawing.Size(83, 28);
+            this.bAdd.TabIndex = 130;
+            this.bAdd.Text = "Add";
+            this.bAdd.UseVisualStyleBackColor = false;
+            this.bAdd.Visible = false;
+            this.bAdd.Click += new System.EventHandler(this.bAdd_Click);
+            // 
+            // pbDot1
+            // 
+            this.pbDot1.Image = ((System.Drawing.Image)(resources.GetObject("pbDot1.Image")));
+            this.pbDot1.Location = new System.Drawing.Point(130, 242);
+            this.pbDot1.Name = "pbDot1";
+            this.pbDot1.Size = new System.Drawing.Size(25, 25);
+            this.pbDot1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbDot1.TabIndex = 124;
+            this.pbDot1.TabStop = false;
+            this.pbDot1.Visible = false;
+            this.pbDot1.Click += new System.EventHandler(this.pbDot1_Click);
+            // 
+            // pbDot3
+            // 
+            this.pbDot3.Image = ((System.Drawing.Image)(resources.GetObject("pbDot3.Image")));
+            this.pbDot3.Location = new System.Drawing.Point(180, 242);
+            this.pbDot3.Name = "pbDot3";
+            this.pbDot3.Size = new System.Drawing.Size(25, 25);
+            this.pbDot3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbDot3.TabIndex = 123;
+            this.pbDot3.TabStop = false;
+            this.pbDot3.Visible = false;
+            this.pbDot3.Click += new System.EventHandler(this.pbDot3_Click);
+            // 
+            // pbDot2
+            // 
+            this.pbDot2.Image = ((System.Drawing.Image)(resources.GetObject("pbDot2.Image")));
+            this.pbDot2.Location = new System.Drawing.Point(155, 242);
+            this.pbDot2.Name = "pbDot2";
+            this.pbDot2.Size = new System.Drawing.Size(25, 25);
+            this.pbDot2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbDot2.TabIndex = 122;
+            this.pbDot2.TabStop = false;
+            this.pbDot2.Visible = false;
+            this.pbDot2.Click += new System.EventHandler(this.pbDot2_Click);
             // 
             // tbValidThru
             // 
@@ -153,16 +234,53 @@
             this.pbLargeCard.TabIndex = 1;
             this.pbLargeCard.TabStop = false;
             // 
-            // Card
+            // AddDeleteCard
+            // 
+            this.AddDeleteCard.BackColor = System.Drawing.Color.White;
+            this.AddDeleteCard.Font = new System.Drawing.Font("Helvetica", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.AddDeleteCard.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.addToolStripMenuItem,
+            this.deleteToolStripMenuItem});
+            this.AddDeleteCard.Name = "contextMenuStrip1";
+            this.AddDeleteCard.Size = new System.Drawing.Size(181, 70);
+            // 
+            // addToolStripMenuItem
+            // 
+            this.addToolStripMenuItem.Font = new System.Drawing.Font("Helvetica", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.addToolStripMenuItem.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(17)))), ((int)(((byte)(23)))), ((int)(((byte)(41)))));
+            this.addToolStripMenuItem.ImageScaling = System.Windows.Forms.ToolStripItemImageScaling.None;
+            this.addToolStripMenuItem.Name = "addToolStripMenuItem";
+            this.addToolStripMenuItem.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.addToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.addToolStripMenuItem.Text = "Add";
+            this.addToolStripMenuItem.TextAlign = System.Drawing.ContentAlignment.BottomLeft;
+            this.addToolStripMenuItem.Click += new System.EventHandler(this.addToolStripMenuItem_Click);
+            // 
+            // deleteToolStripMenuItem
+            // 
+            this.deleteToolStripMenuItem.Font = new System.Drawing.Font("Helvetica", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.deleteToolStripMenuItem.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(17)))), ((int)(((byte)(23)))), ((int)(((byte)(41)))));
+            this.deleteToolStripMenuItem.ImageScaling = System.Windows.Forms.ToolStripItemImageScaling.None;
+            this.deleteToolStripMenuItem.Name = "deleteToolStripMenuItem";
+            this.deleteToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.deleteToolStripMenuItem.Text = "Delete";
+            this.deleteToolStripMenuItem.Click += new System.EventHandler(this.deleteToolStripMenuItem_Click);
+            // 
+            // CardBar
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.Controls.Add(this.groupBox1);
-            this.Name = "Card";
+            this.Name = "CardBar";
             this.Size = new System.Drawing.Size(340, 276);
             this.groupBox1.ResumeLayout(false);
             this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pbAdditionaSettings)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbDot1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbDot3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbDot2)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pbLargeCard)).EndInit();
+            this.AddDeleteCard.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -178,5 +296,13 @@
         private System.Windows.Forms.Label lCardTitle1;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.PictureBox pbLargeCard;
+        private System.Windows.Forms.PictureBox pbDot1;
+        private System.Windows.Forms.PictureBox pbDot3;
+        private System.Windows.Forms.PictureBox pbDot2;
+        private System.Windows.Forms.PictureBox pbAdditionaSettings;
+        private System.Windows.Forms.Button bAdd;
+        private System.Windows.Forms.ContextMenuStrip AddDeleteCard;
+        private System.Windows.Forms.ToolStripMenuItem addToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem deleteToolStripMenuItem;
     }
 }
--- Finance-tracker/Controls/CardBar.cs
+++ Finance-tracker/Controls/CardBar.cs
@@ -3,11 +3,12 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Dynamic;
 using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Text.RegularExpressions;
 using System.Windows.Forms;
+using Finance_tracker.Entity_classes;
 
 
 namespace Finance_tracker.Controls
@@ -15,19 +16,9 @@ namespace Finance_tracker.Controls
     public partial class CardBar : UserControl
     {
         string projectPath = Path.GetFullPath(Path.Combine(Application.StartupPath, "..\\.."));
-
-        public CardBar()
-        {
-            InitializeComponent();
-        }
-
-        public void FillFields(string number, string fullName, string validThru)
-        {
-            var cardNumber = string.Concat("**** **** **** ", number.Substring(12));
-            tbCardNumber.Text = cardNumber;
-            tbCardHolder.Text = fullName;
-            tbValidThru.Text = validThru;
-        }
+        int indexSelectedCard;
+        public int UserId { get; set; }
+        public CardBalance CardBalance { get; set; }
 
         public string CardNumber
         { get { return tbCardNumber.Text; } }
@@ -55,6 +46,27 @@ namespace Finance_tracker.Controls
 
         public bool TbValidThruReadOnly
         { set { tbValidThru.ReadOnly = value; } }
+
+        public int IndexSelectedCard
+        {
+            get { return indexSelectedCard; }
+            set { indexSelectedCard = value; }
+        }
+
+        public event EventHandler CardClick;
+
+        public CardBar()
+        {
+            InitializeComponent();
+        }
+
+        public void FillFields(string number, string fullName, string validThru)
+        {
+            var cardNumber = string.Concat("**** **** **** ", number.Substring(12));
+            tbCardNumber.Text = cardNumber;
+            tbCardHolder.Text = fullName;
+            tbValidThru.Text = validThru;
+        }
 
         public void ColorCardBlue()
         {
@@ -88,6 +100,220 @@ namespace Finance_tracker.Controls
             tbCardNumber.Clear();
             tbValidThru.Clear();
             tbCardHolder.Clear();
+        }
+
+        public bool CheckCorrectCardData()
+        {
+            string cardNumber = tbCardNumber.Text;
+            var cardHolder = tbCardHolder.Text;
+            string validThru = tbValidThru.Text;
+            int numberOfWordsInName = cardHolder.Split(' ').Length;
+
+            bool eror = false;
+            string erorText = string.Empty;
+
+            if (cardNumber.Length != 16)
+            {
+                erorText = "Invalid card number";
+                eror = true;
+            }
+
+            else if (numberOfWordsInName != 2)
+            {
+                erorText = "Invalid card holder";
+                eror = true;
+            }
+
+            else if (!Regex.IsMatch(validThru, @"^(0[1-9]|1[0-2])\/[0-9]{2}$"))
+            {
+                erorText = "Invalid valid thru";
+                eror = true;
+            }
+
+            if (eror)
+            {
+                MessageBox.Show(erorText);
+                return false;
+            }
+
+            return true;
+        }
+
+        public void CountOfDots(int count)
+        {
+            pbDot1.Visible = false;
+            pbDot2.Visible = false;
+            pbDot3.Visible = false;
+
+            switch (count)
+            {
+                case 1:
+                    pbDot1.Visible = true;
+                    pbDot1.Location = new Point(155, 242);
+                    break;
+                case 2:
+                    pbDot1.Visible = true;
+                    pbDot1.Location = new Point(142, 242);
+                    pbDot2.Visible = true;
+                    pbDot2.Location = new Point(167, 242);
+                    break;
+                case 3:
+                    pbDot1.Visible = true;
+                    pbDot1.Location = new Point(130, 242);
+                    pbDot2.Visible = true;
+                    pbDot2.Location = new Point(155, 242);
+                    pbDot3.Visible = true;
+                    pbDot3.Location = new Point(180, 242);
+                    break;
+            }
+        }
+
+        public void SetSelectedCard(int dotNumber)
+        {
+            string grayDotPath = Path.Combine(projectPath, "Icons\\dot_icon.png");
+            string blueDotPath = Path.Combine(projectPath, "Icons\\selected_dot_icon.png");
+
+            pbDot1.Image = Image.FromFile(grayDotPath);
+            pbDot2.Image = Image.FromFile(grayDotPath);
+            pbDot3.Image = Image.FromFile(grayDotPath);
+
+            switch (dotNumber)
+            {
+                case 0:
+                    indexSelectedCard = 0;
+                    pbDot1.Image = Image.FromFile(blueDotPath);
+                    break;
+                case 1:
+                    indexSelectedCard = 1;
+                    pbDot2.Image = Image.FromFile(blueDotPath);
+                    break;
+                case 2:
+                    indexSelectedCard = 2;
+                    pbDot3.Image = Image.FromFile(blueDotPath);
+                    break;
+            }
+        }
+
+        private void pbDot1_Click(object sender, EventArgs e)
+        {
+            SetSelectedCard(0);
+            CardClick?.Invoke(this, EventArgs.Empty);
+        }
+
+        private void pbDot2_Click(object sender, EventArgs e)
+        {
+            SetSelectedCard(1);
+            CardClick.Invoke(this, EventArgs.Empty);
+        }
+
+        private void pbDot3_Click(object sender, EventArgs e)
+        {
+            SetSelectedCard(2);
+            CardClick?.Invoke(this, EventArgs.Empty);
+        }
+
+        private void pbAdditionaSettings_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                var location = new Point(
+                    e.Location.X - 130,
+                    e.Location.Y
+                );
+
+                AddDeleteCard.Show(pbAdditionaSettings, location);
+            }
+        }
+
+        private void addToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            int countOfCard;
+            using (var context = new FinanceTrackerContext())
+            {
+                var cards = context.Cards.Where(x => x.UserId == UserId).ToList();
+                countOfCard = cards.Count;
+            }
+
+            if (countOfCard < 3)
+            {
+                CountOfDots(0);
+                ColorCardGray();
+                CardBalance.ClearBalance();
+
+                TbNumberBackColor = Color.FromArgb(183, 192, 201);
+                TbHolderBackColor = Color.FromArgb(183, 192, 201);
+                TbValidThruBackColor = Color.FromArgb(183, 192, 201);
+                CardBalance.TbBackColor = Color.FromArgb(183, 192, 201);
+
+                TbNumberReadOnly = false;
+                TbHolderReadOnly = false;
+                TbValidThruReadOnly = false;
+                CardBalance.TbReadOnly = false;
+                bAdd.Visible = true;
+
+            }
+            else
+                MessageBox.Show("There can be only 3 cards");
+        }
+
+        private void deleteToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            using (var context = new FinanceTrackerContext())
+            {
+                var cardNumber = CardNumber;
+
+                var deleteCard = context.Cards
+                    .Where(x => x.UserId == UserId
+                             && x.Number == cardNumber)
+                    .FirstOrDefault();
+
+                if (deleteCard != null)
+                {
+                    context.Cards.Remove(deleteCard);
+                    context.SaveChanges();
+                }
+            }
+
+            if (IndexSelectedCard > 0)
+                IndexSelectedCard--;
+
+            CardClick?.Invoke(this, EventArgs.Empty);
+        }
+
+        private void bAdd_Click(object sender, EventArgs e)
+        {
+            string balance = CardBalance.Balance;
+            bool correctCardData = CheckCorrectCardData();
+            bool correctBalance = CardBalance.CheckCorrectBalance();
+
+            if (correctCardData && correctBalance)
+            {
+                TbNumberReadOnly = true;
+                TbHolderReadOnly = true;
+                TbValidThruReadOnly = true;
+                CardBalance.TbReadOnly = true;
+                bAdd.Visible = true;
+
+                CardBalance.TbBackColor = Color.White;
+
+                var card = new Card
+                {
+                    Number = CardNumber,
+                    HolderFullName = CardHolder,
+                    ValidThru = ValidThru,
+                    Balance = Convert.ToDecimal(balance),
+                    UserId = UserId
+                };
+
+                using (var context = new FinanceTrackerContext())
+                {
+                    context.Cards.Add(card);
+                    context.SaveChanges();
+                }
+
+                IndexSelectedCard++;
+                CardClick?.Invoke(this, EventArgs.Empty);
+            }
         }
     }
 }
