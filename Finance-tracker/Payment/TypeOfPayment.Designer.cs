b98c209a18001119116d361033c7270cc65d8697
rigbeats <rishat.gabdullin2016@mail.ru>
2023-09-29 21:25:43 +0600

    контрол - кнопка выбора типа перевода

--- Finance-tracker/Payment/TypeOfPayment.Designer.cs
+++ Finance-tracker/Payment/TypeOfPayment.Designer.cs
@@ -28,63 +28,99 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.groupBox11 = new System.Windows.Forms.GroupBox();
-            this.title = new System.Windows.Forms.TextBox();
             this.image = new System.Windows.Forms.PictureBox();
-            this.groupBox11.SuspendLayout();
+            this.panel6 = new System.Windows.Forms.Panel();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.title = new System.Windows.Forms.TextBox();
             ((System.ComponentModel.ISupportInitialize)(this.image)).BeginInit();
             this.SuspendLayout();
             // 
-            // groupBox11
-            // 
-            this.groupBox11.BackColor = System.Drawing.Color.White;
-            this.groupBox11.Controls.Add(this.image);
-            this.groupBox11.Controls.Add(this.title);
-            this.groupBox11.Location = new System.Drawing.Point(1, 1);
-            this.groupBox11.Name = "groupBox11";
-            this.groupBox11.Size = new System.Drawing.Size(95, 95);
-            this.groupBox11.TabIndex = 123;
-            this.groupBox11.TabStop = false;
-            // 
-            // title
-            // 
-            this.title.BorderStyle = System.Windows.Forms.BorderStyle.None;
-            this.title.Font = new System.Drawing.Font("Helvetica Bold", 12F);
-            this.title.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(36)))), ((int)(((byte)(95)))), ((int)(((byte)(232)))));
-            this.title.Location = new System.Drawing.Point(1, 67);
-            this.title.Name = "title";
-            this.title.Size = new System.Drawing.Size(93, 20);
-            this.title.TabIndex = 121;
-            this.title.Text = "title";
-            this.title.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
-            // 
             // image
             // 
-            this.image.Location = new System.Drawing.Point(25, 18);
+            this.image.Location = new System.Drawing.Point(26, 19);
             this.image.Name = "image";
             this.image.Size = new System.Drawing.Size(45, 45);
             this.image.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-            this.image.TabIndex = 119;
+            this.image.TabIndex = 122;
             this.image.TabStop = false;
+            this.image.Click += new System.EventHandler(this.TypeOfPayment_Click);
+            // 
+            // panel6
+            // 
+            this.panel6.BackColor = System.Drawing.Color.Gainsboro;
+            this.panel6.Location = new System.Drawing.Point(1, 7);
+            this.panel6.Name = "panel6";
+            this.panel6.Size = new System.Drawing.Size(95, 1);
+            this.panel6.TabIndex = 124;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Gainsboro;
+            this.panel1.Location = new System.Drawing.Point(1, 94);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(112, 1);
+            this.panel1.TabIndex = 125;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.Gainsboro;
+            this.panel2.Location = new System.Drawing.Point(1, 7);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(1, 88);
+            this.panel2.TabIndex = 126;
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.Color.Gainsboro;
+            this.panel3.Location = new System.Drawing.Point(95, 7);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(1, 88);
+            this.panel3.TabIndex = 127;
+            // 
+            // title
+            // 
+            this.title.BackColor = System.Drawing.Color.White;
+            this.title.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.title.Font = new System.Drawing.Font("Helvetica Bold", 12F);
+            this.title.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(36)))), ((int)(((byte)(95)))), ((int)(((byte)(232)))));
+            this.title.Location = new System.Drawing.Point(2, 67);
+            this.title.Name = "title";
+            this.title.ReadOnly = true;
+            this.title.Size = new System.Drawing.Size(93, 20);
+            this.title.TabIndex = 128;
+            this.title.Text = "title";
+            this.title.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.title.Click += new System.EventHandler(this.TypeOfPayment_Click);
             // 
             // TypeOfPayment
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Controls.Add(this.groupBox11);
+            this.BackColor = System.Drawing.Color.White;
+            this.Controls.Add(this.title);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.panel6);
+            this.Controls.Add(this.image);
             this.Name = "TypeOfPayment";
             this.Size = new System.Drawing.Size(97, 97);
-            this.groupBox11.ResumeLayout(false);
-            this.groupBox11.PerformLayout();
+            this.Click += new System.EventHandler(this.TypeOfPayment_Click);
             ((System.ComponentModel.ISupportInitialize)(this.image)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.GroupBox groupBox11;
         private System.Windows.Forms.PictureBox image;
+        private System.Windows.Forms.Panel panel6;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel3;
         private System.Windows.Forms.TextBox title;
     }
 }
--- Finance-tracker/Payment/TypeOfPayment.cs
+++ Finance-tracker/Payment/TypeOfPayment.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Runtime.Remoting.Messaging;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -13,6 +14,7 @@ namespace Finance_tracker.Payment
 {
     public partial class TypeOfPayment : UserControl
     {
+        public event Action<string> OpenTransaction;
         public Image PictureBoxImage
         { 
             get { return image.Image; }
@@ -28,6 +30,11 @@ namespace Finance_tracker.Payment
         public TypeOfPayment()
         {
             InitializeComponent();
+        }
+
+        private void TypeOfPayment_Click(object sender, EventArgs e)
+        {
+            OpenTransaction?.Invoke(title.Text);
         }
     }
 }
